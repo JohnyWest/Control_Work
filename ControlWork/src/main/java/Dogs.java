9c3d4e89830c72618fa26ea1db872bc272e1329a
Johny_West <bbassed@gmail.com>
2024-03-01 16:47:12 +0500

    Added programm

---
+++ ControlWork/src/main/java/Dogs.java
@@ -0,0 +1,9 @@
+public class Dogs extends HomeAnimals {
+    public Dogs(String name, int age) {
+        super(name, age);
+    }
+
+    public void bark() {
+        System.out.println("Гав-Гав!");
+    }
+}
