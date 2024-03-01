0fa1b23508d27db7f06ba206f53e93315a26e44c
Johny_West <bbassed@gmail.com>
2024-03-01 16:47:38 +0500

    add Programm

--- ControlWork/src/main/java/HomeAnimals.java
+++ ControlWork/src/main/java/HomeAnimals.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HomeAnimals {
+public class HomeAnimals {
+    private String name;
+    private int age;
+
+    public HomeAnimals(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void say() {
+        System.out.println("Животное издает звук");
+    }
 }
--- ControlWork/src/main/java/Program.java
+++ ControlWork/src/main/java/Program.java
@@ -1,2 +1,39 @@
-public class Programm {
+import java.util.Scanner
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean работаЗавершена = false;
+
+        while (!работаЗавершена) {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Завести новое животное");
+            System.out.println("2. Определить животное в правильный класс");
+            System.out.println("3. Увидеть список команд, которое выполняет животное");
+            System.out.println("4. Обучить животное новым командам");
+            System.out.println("5. Выход");
+
+            int choose = scanner.nextInt();
+            switch (choose) {
+                case 1:
+                    // Логика для заведения нового животного
+                    break;
+                case 2:
+                    // Логика для определения животного в правильный класс
+                    break;
+                case 3:
+                    // Логика для отображения списка команд, которые выполняет животное
+                    break;
+                case 4:
+                    // Логика для обучения животного новым командам
+                    break;
+                case 5:
+                    работаЗавершена = true;
+                    break;
+                default:
+                    System.out.println("Некорректный выбор, выберите снова. ");
+            }
+        }
+        System.out.println("Работа завершена");
+    }
 }
